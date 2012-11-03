8ad1e9c91c08c60a2a6f01651ba0fb3e639a13cc
Chris Page <phriscage@gmail.com>
2012-11-03 19:53:57 +0000

    adding factorial

---
+++ factorial/factorial.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python2.7
+""" return a factorial from a given number """
+import sys
+import math
+
+
+def get_factorial(number):
+    """ iterate over a given number and return the factorial 
+    Args:
+        number (int): number integer
+    Returns:
+        factorial integer
+    """
+    factorial = 1
+    for n in range(int(number), 1, -1):
+        factorial *= n
+    return factorial
+
+
+def math_factorial(number):
+    """ user the built-in math.factorial to return factorial 
+    Args:
+        number (int): number integer
+    Returns:
+        factorial integer
+    """
+    return math.factorial(int(number))
+
+    
+def main():
+    """ run the main logic """
+    #number = raw_input()
+    if len(sys.argv) < 2:
+        print "Argument required."
+        sys.exit(1)
+    number = sys.argv[1]
+    print get_factorial(number)
+    print math_factorial(number)
+    
+if __name__ == '__main__':
+    main()
+
