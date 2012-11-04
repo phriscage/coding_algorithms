b2c7c49259452df5eeafd62d91ebf2364426e777
Chris Page <phriscage@gmail.com>
2012-11-04 04:32:00 +0000

    modified binary_search for left and right bound

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python2.7
 """ 
     this can be easily accomplished by list.index(?) but included a 
-    straight-forward example as well.
+    straight-forward example as well. lower-bound left to right and
+    right to left need to be taken into account.
 """
 
 def binary_search(values, target):
     """ binary search is recursive while loop to locate the target in 
-        the list 
+        the list and we need to search both directions.
     Args:
         values (list): list of integers
         target (int): target within values
@@ -14,9 +15,7 @@ def binary_search(values, target):
         middle (int)
     """
     low = 0
-    if type(values) is not list:
-        return False
-    high = len(values)
+    high = len(values) - 1
     while low <= high:
         middle = low + (high-low) / 2
         if values[middle] == int(target):
@@ -25,6 +24,17 @@ def binary_search(values, target):
             low = middle + 1
         else:
             high = middle - 1
+    low = 0
+    high = len(values) - 1
+    while low <= high:
+        middle = low + (high-low) / 2
+        if values[middle] == int(target):
+            return middle
+        elif values[middle] >= int(target):
+            low = middle + 1
+        else:
+            high = middle - 1
+    raise ValueError, "%i is not in list" % target
 
 
 def main():
@@ -32,8 +42,14 @@ def main():
     values = [1,2,4,7,9,11,1,23,34,55,66]
     print binary_search(values, 23)
     print values.index(23)
-    print binary_search(values, 1)
-    print values.index(1)
+    values = sorted(range(10), reverse=True)
+    for i in range(10):
+        if not binary_search(values, i) == values.index(i):
+            print i, False
+    for i in sorted(range(10), reverse=True):
+        if not binary_search(values, i) == values.index(i):
+            print i, False 
+    print True
 
 
 if __name__ == '__main__':
--- depth_first_search/depth_first_search.py
+++ depth_first_search/depth_first_search.py
@@ -5,18 +5,14 @@
 """
 
 def recursive(graph, start, path=[]):
-    """ 
-        recursive depth first search from start 
-
+    """ recursive depth first search from start 
     Args:
-        graph: dict of graph
-        start: string starting value
-        path: list of values in path
-
+        graph (dict): graph dict
+        start (str): starting value str
+        path (list): list of values in path
     Returns:
-        path: list of values in path
+        path (list): list of values in path
     """
-
     path = path + [start]
     for node in graph[start]:
         if not node in path:
@@ -25,23 +21,19 @@ def recursive(graph, start, path=[]):
 
 
 def find_all_paths(graph, start, end, path=[]):
-    """ 
-        locate each path in dict of lists 
-
+    """ locate each path in dict of lists 
     Args:
-        graph: dict of graph
-        start: string start value
-        start: string end value
-        path: list of values in path
-
+        graph (dict): graph dict
+        start (str): starting value str
+        end (str): end value str
+        path (list): list of values in path
     Returns:
-        paths: list of list of values in path
+        paths (list): list of list of values in path
     """
-
     path = path + [start]
     if start == end:
         return [path]
-    if not graph.has_key(start):
+    if start not in graph:
         return []
     paths = []
     for node in graph[start]:
@@ -53,8 +45,7 @@ def find_all_paths(graph, start, end, path=[]):
 
 
 if __name__ == '__main__':
-    """
-        there are redundant paths from UR1, UR2 to ER1 and ER2:
+    """ there are redundant paths from UR1, UR2 to ER1 and ER2:
 
         AR-->SUR->UR3
              /    | 
@@ -64,9 +55,7 @@ if __name__ == '__main__':
             
     """
     graph = {'AR': ['SUR'], 'SUR': ['UR1', 'UR3'], 'UR1': ['ER1', 'ER2'], 'UR2': ['ER1', 'ER2'], 'UR3': ['UR2'], 'ER1': [], 'ER2': ['CR'], 'CR': []}
-    print recursive(graph, 'AR')
-    print find_all_paths(graph, 'AR', 'CR')
-    print min(find_all_paths(graph, 'AR', 'CR'), key=len)
+    print "recursive: ", recursive(graph, 'AR')
+    print "find_all_paths: ", find_all_paths(graph, 'AR', 'CR')
+    print "find_all_paths (min): ", min(find_all_paths(graph, 'AR', 'CR'), key=len)
     
-        
-
